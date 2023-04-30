52d3e7f78bec7c83a55c1c61f78a906dc38c27d4
J <jcmmprogramacion@gmail.com>
2023-04-30 21:21:50 +0200

    add to folders

--- js/demo.js
+++ js/demo.js
@@ -1,10 +1,9 @@
 document.getElementById("saludoConsola").value = "Hola, mira mi saludo en Console";
 document.getElementById("inputValorConsola").value = "3+24";
-document.getElementById("nameToInput").value = "";
+document.getElementById("nameToInput").value;
 document.getElementById('inputToP').innerText;
 document.getElementById("button").value;
 document.getElementById("codeImage").src;
-
 
 // link imagen
 // https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hostinger.es%2Ftutoriales%2Fmejores-sitios-para-aprender-a-programar-gratis&psig=AOvVaw1GMPHOe7SR3Qs2n9_BCsl8&ust=1682935206498000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCIjT7LCs0f4CFQAAAAAdAAAAABAD
