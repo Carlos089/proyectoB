3a9bf77f828b8dd835c09daaf8481c7ba7e8f07f
J <jcmmprogramacion@gmail.com>
2023-05-04 11:33:33 +0200

    end bonus project

--- js/demo.js
+++ js/demo.js
@@ -5,6 +5,22 @@ document.getElementById('inputToP').innerText;
 document.getElementById("button").value;
 document.getElementById("codeImage").src;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // link imagen
 // https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hostinger.es%2Ftutoriales%2Fmejores-sitios-para-aprender-a-programar-gratis&psig=AOvVaw1GMPHOe7SR3Qs2n9_BCsl8&ust=1682935206498000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCIjT7LCs0f4CFQAAAAAdAAAAABAD
 
